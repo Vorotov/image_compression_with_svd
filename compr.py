00e2041860ca214a3314b3ad55e9b91d6ee6a167
Vorotov <64152947+Vorotov@users.noreply.github.com>
2020-10-22 10:42:17 +0300

    Update and rename lab3_dop.py to compr.py

--- compr.py
+++ compr.py
@@ -1,28 +1,6 @@
 import numpy as np
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
-img=mpimg.imread('1.jpg')
-RB = np.array(img)
-RB[:, :, 1] = 0
-plt.imshow(RB)
-plt.show()
-
-RB = np.array(img, dtype=int)
-RB += -50
-plt.imshow(RB)
-plt.show()
-
-gray = np.dot(img, [0.2989, 0.5870, 0.1440])
-plt.imshow(gray)
-plt.gray()
-plt.show()
-
-fig, ax = plt.subplots()
-RB = np.array(img[:,:,1])
-RB = RB.ravel()
-ax.hist(RB, bins=256, color="green", range=(0, 256))
-plt.show()
-
 
 def compr(A,r):
     u, s, v = np.linalg.svd(A)
@@ -32,6 +10,7 @@ def compr(A,r):
     v = v[0:r,:]
     return (u.dot(s)).dot(v)
 
+img=mpimg.imread('1.jpg')
 R = img[:, :, 0]
 G = img[:, :, 1]
 B = img[:, :, 2]
